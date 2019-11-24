6f799a1ed748308f157854a5b13a52a358527805
Ezekiel <iamwebwiz@gmail.com>
2019-11-24 07:29:57 +0100

    Added guarded fields array and mutators to Customer model

--- app/Customer.php
+++ app/Customer.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Customer extends Model
 {
     use SoftDeletes;
+
+    /**
+     * Fields to be guarded. An empty signifies all properties are mass-assignable
+     *
+     * @var array $guarded
+     */
+    protected $guarded = [];
+
+    public function setFirstNameAttribute($value): void
+    {
+        $this->attributes['first_name'] = ucfirst($value);
+    }
+
+    public function setLastNameAttribute($value): void
+    {
+        $this->attributes['last_name'] = ucfirst($value);
+    }
 }
