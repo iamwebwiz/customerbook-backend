031f6f374a48dfc7133c2766df52678c5174075d
Ezekiel <iamwebwiz@gmail.com>
2019-11-24 09:32:58 +0100

    Added comments to mutators

--- app/Customer.php
+++ app/Customer.php
@@ -16,11 +16,23 @@ class Customer extends Model
      */
     protected $guarded = [];
 
+    /**
+     * Set customer's first name value
+     *
+     * @param $value
+     * @return void
+     */
     public function setFirstNameAttribute($value): void
     {
         $this->attributes['first_name'] = ucfirst($value);
     }
 
+    /**
+     * Set customer's last name attribute
+     *
+     * @param $value
+     * @return void
+     */
     public function setLastNameAttribute($value): void
     {
         $this->attributes['last_name'] = ucfirst($value);
