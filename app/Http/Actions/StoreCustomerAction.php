b187e98f7d02a3906e737b5e328ee515355fe0f3
Ezekiel <iamwebwiz@gmail.com>
2019-11-24 07:31:17 +0100

    Added logic for storing new customer

--- app/Http/Actions/StoreCustomerAction.php
+++ app/Http/Actions/StoreCustomerAction.php
@@ -4,7 +4,27 @@
 namespace App\Http\Actions;
 
 
+use App\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Traits\HasApiResponse;
+use Illuminate\Http\JsonResponse;
+
 class StoreCustomerAction
 {
+    use HasApiResponse;
 
+    /**
+     * Execute creating a new customer
+     *
+     * @param StoreCustomerRequest $request
+     * @return JsonResponse
+     */
+    public function execute(StoreCustomerRequest $request): JsonResponse
+    {
+        $customer = Customer::create($request->except('_token'));
+
+        return $this->successResponse('Customer created successfully', [
+            'customer' => $customer
+        ]);
+    }
 }
